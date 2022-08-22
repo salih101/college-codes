d441ea3ed8642be076615e4fb899ca997f1696fa
salih101 <95134518+salih101@users.noreply.github.com>
2022-08-22 08:59:18 +0530

    Update 9_short_form.c

--- 9_short_form.c
+++ 9_short_form.c
@@ -4,10 +4,10 @@
 int main(){
 	char str[20];
 	int i=0;
-    printf("Enter the string:");
+        printf("Enter the string:");
 	gets(str);
 	strupr(str);
-    printf("\nShort form:");
+        printf("\nShort form:");
 	printf("%c",str[0]);
 	while(str[i]){
 		if(str[i] == ' ') printf("%c",str[i+1]);
