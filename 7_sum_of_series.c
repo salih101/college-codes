7110f4cd3639c905ee4f00211a3ea22f44585edd
salih101 <95134518+salih101@users.noreply.github.com>
2022-08-26 22:21:30 +0530

    Update 7_sum_of_series.c

--- 7_sum_of_series.c
+++ 7_sum_of_series.c
@@ -2,17 +2,14 @@
 #include<math.h>
 
 int main(){
-double term,sum=0;
-int i=1;
+    double term,sum=0;
+    int i=1;
 
-for(i=1;i<=10;++i)    
-{
-    term=1/pow(i,i);
-    if (term>0.000001){
-        sum+=term;
-        }
-}
-
-printf("Sum of series=%f",sum);
-
+    do
+    {
+        term = 1/pow(i,i);
+        sum += term;
+        i++;
+    } while (term>0.000001);
+    printf("Sum of series=%f",sum);
 }
