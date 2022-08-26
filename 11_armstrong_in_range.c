dbaae50458477574180af98b52651e4e224f8451
salih101 <95134518+salih101@users.noreply.github.com>
2022-08-26 22:32:37 +0530

    Update 11_armstrong_in_range.c

--- 11_armstrong_in_range.c
+++ 11_armstrong_in_range.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 
 int main(){
-	int j,u,i,s,m,d;
+	int a,b,i,s,t,d;
 	printf("Enter the Range:");
-	scanf("%d%d",&j,&u);
-	printf("Amstrong Numbers in the range: %d t0 %d",j,u);
-	for (i=j;i<=u;i++){
+	scanf("%d%d",&a,&b);
+	printf("Amstrong Numbers in the range: %d to %d",a,b);
+	for (i=a;i<=b;i++){
 		s=0;
-		m=i;
-		while(m){
-			d=m%10;
+		t=i;
+		while(t){
+			d=t%10;
 			s=s+d*d*d;
-			m=m/10;
+			t=t/10;
 		}
 		if (i==s){
 			printf("\n%d ",i);
