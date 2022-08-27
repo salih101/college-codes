e2f4c3fb2953bd4046dfe0484f8e7569ed2a4f5c
salih101 <95134518+salih101@users.noreply.github.com>
2022-08-27 06:24:59 +0530

    Update 2_n_fibonacci.c

--- 2_n_fibonacci.c
+++ 2_n_fibonacci.c
@@ -5,7 +5,7 @@ int main()
     int a, b, c, i, n;
     printf("Enter the Limit: ");
     scanf("%d", &n);
-    printf("\nFirst %d fibonacci Numbers are: ", n);
+    printf("\nFirst %d Fibonacci Numbers are: ", n);
     for (a = 1, b = 0, i = 0; i < n; i++)
     {
         c = a + b;
